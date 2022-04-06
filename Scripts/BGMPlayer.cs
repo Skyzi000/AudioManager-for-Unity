a9e3fde41690ddd7632b316432db0523a387a916
Skyzi000 <skyzi000r@gmail.com>
2022-04-07 05:48:01 +0900

    BGMPlayerにいくつかの機能を実装
    ・publicな管理用関数
    ・AudioClipを変更するためのpublicなプロパティ
    ・AudioClip変更時イベント
    ・エディタでのプレイ中にInspector上でプロパティを変更した場合に即時適用

--- Scripts/BGMPlayer.cs
+++ Scripts/BGMPlayer.cs
@@ -1,23 +1,47 @@
 ﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Skyzi000.AudioManager
 {
     /// <summary>
-    /// シーン上に置くだけで、AudioManager経由でBGMを流してくれるやつ
+    /// シーン上に置くだけで、AudioManagerを利用してBGMを流してくれるやつ
     /// </summary>
     public class BGMPlayer : MonoBehaviour
     {
-        [SerializeField, Header("流すBGM"), AssetSelector, Required]
+        /// <summary>
+        /// 再生するBGM
+        /// </summary>
+        public AudioClip AudioClip
+        {
+            get => audioClip;
+            set
+            {
+                audioClip = value;
+                OnAudioClipChanged.Invoke(audioClip);
+            }
+        }
+
+        /// <summary>
+        /// 再生に利用している、<see cref="AudioManager"/>管理下の<see cref="AudioSource"/>
+        /// </summary>
+        /// <remarks>一度も再生していなければnull</remarks>
+        public AudioSource AudioSource { get; private set; }
+
+        [field: SerializeField, Header("AudioClip変更時イベント"), PropertyOrder(10)]
+        public UnityEvent<AudioClip> OnAudioClipChanged { get; private set; } = new UnityEvent<AudioClip>();
+
+
+        [SerializeField, Header("BGM"), AssetSelector, Required]
         private AudioClip audioClip;
 
-        [SerializeField, Header("音量(通常は1)")]
+        [SerializeField, Header("音量")]
         private float volume = 1f;
 
         [SerializeField, Header("ピッチ(再生速度)")]
         private float pitch = 1f;
 
-        [SerializeField, Header("遅延秒数")]
+        [SerializeField, Header("遅延秒数"), Min(0f)]
         private float delay;
 
         [SerializeField, Header("ループ")]
@@ -29,32 +53,62 @@ namespace Skyzi000.AudioManager
         [SerializeField, Range(0, 256), Header("優先度(0が最高、256が最低)")]
         private int priority = AudioManager.BGMDefaultPriority;
 
-        private AudioSource _audioSource;
+
+        public void Play()
+        {
+            if (AudioClip != null)
+                AudioSource = BGM.Play(AudioClip, volume, pitch, delay, loop, allowsDuplicate, priority);
+        }
+
+        public void Pause()
+        {
+            // isPlayingをtrueに保つためAudioSource.Pause()は使っていない
+            if (AudioClip != null)
+                AudioSource.pitch = 0f;
+        }
+
+        public void UnPause()
+        {
+            if (AudioClip != null)
+                AudioSource.pitch = pitch;
+        }
+
+        public void Stop()
+        {
+            if (AudioClip != null)
+                BGM.Stop(AudioClip);
+        }
 
         private void OnEnable()
         {
-            if (audioClip == null)
-                return;
-            if (_audioSource == null)
+            if (AudioSource == null || AudioSource.clip != AudioClip)
             {
-                _audioSource = BGM.Play(audioClip, volume, pitch, delay, loop, allowsDuplicate, priority);
+                Play();
             }
             else
             {
-                _audioSource.pitch = pitch;
+                UnPause();
             }
         }
 
-        private void OnDisable()
-        {
-            if (audioClip != null)
-                _audioSource.pitch = 0f;
-        }
+        private void OnDisable() => Pause();
 
-        private void OnDestroy()
+        private void OnDestroy() => Stop();
+
+#if UNITY_EDITOR
+        private void OnValidate()
         {
-            if (audioClip != null)
-                BGM.Stop(audioClip);
+            if (!UnityEditor.EditorApplication.isPlaying)
+                return;
+            if (AudioSource == null || AudioSource.clip != AudioClip)
+                Play();
+            if (AudioSource == null)
+                return;
+            AudioSource.volume = volume;
+            AudioSource.pitch = pitch;
+            AudioSource.loop = loop;
+            AudioSource.priority = priority;
         }
+#endif
     }
 }
