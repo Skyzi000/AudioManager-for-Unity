5aecf7499af2cfa1d613ee76faa8952dd1f0b085
Skyzi000 <skyzi000r@gmail.com>
2023-11-11 21:35:59 +0900

    Can**プロパティを追加し、BGMPlayerのInspector上での操作性を改善

--- Scripts/BGMPlayer.cs
+++ Scripts/BGMPlayer.cs
@@ -31,8 +31,10 @@ namespace Skyzi000.AudioManager
         [field: SerializeField, Tooltip("AudioClip変更時イベント"), PropertyOrder(10)]
         public UnityEvent<AudioClip> OnAudioClipChanged { get; private set; } = new UnityEvent<AudioClip>();
 
-
-        [SerializeField, AssetSelector, RequiredIn(PrefabKind.InstanceInScene)]
+        [FolderPath, SerializeField, Tooltip("AudioClipのあるディレクトリ（AssetListの絞り込みに利用）")]
+        private string directoryPath = "";
+        
+        [SerializeField, AssetList(CustomFilterMethod = nameof(DirectoryPathFilter)), RequiredIn(PrefabKind.InstanceInScene), InlineEditor(InlineEditorModes.LargePreview)]
         private AudioClip audioClip;
 
         [SerializeField, Tooltip("音量")]
@@ -53,13 +55,26 @@ namespace Skyzi000.AudioManager
         [SerializeField, Range(0, 256), Tooltip("優先度(0が最高、256が最低)")]
         private int priority = AudioManager.BGMDefaultPriority;
 
+        public bool CanPlay => Application.isPlaying && AudioClip != null && BGM.FindSource(AudioClip) == null;
+        public bool CanPause => Application.isPlaying && AudioClip != null && AudioSource != null && AudioSource.isPlaying && !Mathf.Approximately(AudioSource.pitch, 0f);
+        public bool CanUnPause => Application.isPlaying && AudioClip != null && AudioSource != null && AudioSource.isPlaying && Mathf.Approximately(AudioSource.pitch, 0f);
+        public bool CanStop => Application.isPlaying && AudioClip != null && AudioSource != null && AudioSource.isPlaying;
 
+        private bool DirectoryPathFilter(Object obj)
+            => string.IsNullOrEmpty(directoryPath)
+#if UNITY_EDITOR
+               || UnityEditor.AssetDatabase.GetAssetPath(obj).StartsWith(directoryPath)
+#endif
+        ;
+        
+        [Button(ButtonSizes.Large, Icon = SdfIconType.PlayFill), HideLabel, ButtonGroup, ShowIf(nameof(CanPlay), false)]
         public void Play()
         {
             if (AudioClip != null)
                 AudioSource = BGM.Play(AudioClip, volume, pitch, delay, loop, allowsDuplicate, priority);
         }
 
+        [Button(ButtonSizes.Large, Icon = SdfIconType.PauseFill), HideLabel, ButtonGroup, ShowIf(nameof(CanPause), false)]
         public void Pause()
         {
             // isPlayingをtrueに保つためAudioSource.Pause()は使っていない
@@ -67,12 +82,14 @@ namespace Skyzi000.AudioManager
                 AudioSource.pitch = 0f;
         }
 
+        [Button(ButtonSizes.Large, Icon = SdfIconType.PlayCircle), HideLabel, ButtonGroup, ShowIf(nameof(CanUnPause), false)]
         public void UnPause()
         {
             if (AudioClip != null && AudioSource != null)
                 AudioSource.pitch = pitch;
         }
 
+        [Button(ButtonSizes.Large, Icon = SdfIconType.StopFill), HideLabel, ButtonGroup, ShowIf(nameof(CanStop), false)]
         public void Stop()
         {
             if (AudioClip != null)
